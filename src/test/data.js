49733e30336fe9cc06476867b21f3073b345eed3
Kirisanth Subramaniam <kirisanth.s@gmail.com>
2017-04-11 20:30:43 -0400

    Test data

---
+++ src/test/data.js
@@ -0,0 +1,114 @@
+const data = [
+    {
+        name: "Math",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Test",
+                mark: 100,
+                weight: 10
+            },
+            {
+                type: "Project",
+                mark: 90,
+                weight: 10
+            },
+            {
+                type: "Test",
+                mark: 80,
+                weight: 10
+            },
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    },
+    {
+        name: "English",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 50
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    },
+    {
+        name: "Science",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    },
+    {
+        name: "Geography",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    },
+    {
+        name: "History",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    },
+    {
+        name: "French",
+        assessments: [
+            {
+                type: "Test",
+                mark: 70,
+                weight: 10
+            },
+            {
+                type: "Exam",
+                mark: 50,
+                weight: 50
+            }
+        ]
+    }
+];
+
+export default data;

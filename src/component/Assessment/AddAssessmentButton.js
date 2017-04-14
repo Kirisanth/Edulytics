9e5f9ae702e94524e29e7de3a822b0004f8abfd7
Kirisanth Subramaniam <kirisanth.s@gmail.com>
2017-04-14 10:38:53 -0400

    Hooked up mark and wight input to add assessment functionality

--- src/component/Assessment/AddAssessmentButton.js
+++ src/component/Assessment/AddAssessmentButton.js
@@ -5,12 +5,16 @@ import { addAssessment } from '../../actions/actions';
 class AddAssessmentButton extends React.Component {
     handleSubmit() {
         const { dispatch, index } = this.props;
-        dispatch(addAssessment(index, this.refs.course.value, 10, 10));
+        const { course, mark, weight } = this.refs;
+
+        dispatch(addAssessment(index, course.value, mark.value, weight.value));
     }
     render() {
         return (
             <div>
-                <input ref="course"/>
+                <input ref="course" placeholder="Assessment"/>
+                <input ref="mark" placeholder="Mark"/>
+                <input ref="weight" placeholder="Weight"/>
                 <div onClick={this.handleSubmit.bind(this)}>AddButton</div>
                 <button>Test</button>
             </div>
